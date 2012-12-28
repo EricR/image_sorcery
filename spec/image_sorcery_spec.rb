8e6ae133dbea5e8c1b05899b2f8bf97b7014a4a7
Matthew Rothenberg <mrothenberg@gmail.com>
2012-12-28 15:23:54 -0500

    use shared examples to eliminate duplicate code

--- spec/image_sorcery_spec.rb
+++ spec/image_sorcery_spec.rb
@@ -1,32 +1,11 @@
 require 'fileutils'
 require 'image_sorcery'
 
-describe "Image Sorcery" do
+describe "Image Sorcery with ImageMagick" do
   before :each do
     FileUtils.copy "./spec/fixtures/dog.jpeg", "./spec/fixtures/dog-2.jpeg" 
     @image = Sorcery.new("./spec/fixtures/dog-2.jpeg") # Who doesn't love dogs?
   end
-  describe "getting the dimensions of an image" do
-    it "returns a hash of dimensions" do
-      @image.dimensions.should == {:x => "160", :y => "120"}
-    end
-  end
-  describe "manipulating an image" do
-    it "resizes an image" do
-      original_dimensions = @image.dimensions
-      @image.manipulate!(:resize => "50%")
-      @image.dimensions.map {|k,v| v.to_i}.should == original_dimensions.map {|k,v| v.to_i/2}
-    end
-  end
-  describe "converting an image" do
-    it "writes the new image out to a file" do
-      @image.convert("new_image.png")
-      File.exists?("new_image.png").should be_true
-    end
-  end
-  describe "identifying an image" do
-    it "returns a list of layers" do
-      @image.identify.should include "JPEG 160x120"
-    end
-  end
+  
+  it_behaves_like Sorcery
 end
