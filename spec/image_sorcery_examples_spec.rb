84769fbcb0b9855663e942f48f69791544772ad6
Tobias Maier <info@tobiasmaier.info>
2012-12-30 01:01:06 +0100

    Use subject in specs

--- spec/image_sorcery_examples_spec.rb
+++ spec/image_sorcery_examples_spec.rb
@@ -1,37 +1,41 @@
 require 'fileutils'
 require 'image_sorcery'
 
-shared_examples_for Sorcery do
+shared_examples_for Sorcery do |new_instance_method|
+  subject(:image) do
+    FileUtils.copy "./spec/fixtures/dog.jpeg", "./spec/fixtures/dog-2.jpeg"
+    Sorcery.send(new_instance_method, "./spec/fixtures/dog-2.jpeg") # Who doesn't love dogs?
+  end
 
   describe "getting the dimensions of an image" do
     it "returns a hash of dimensions" do
-      @image.dimensions.should == {:x => 160, :y => 120}
+      image.dimensions.should == {:x => 160, :y => 120}
     end
   end
 
   describe "manipulating an image" do
     it "resizes an image" do
-      original_dimensions = @image.dimensions
-      @image.manipulate!(:resize => "50%")
-      @image.dimensions.map {|k,v| v.to_i}.should == original_dimensions.map {|k,v| v.to_i/2}
+      original_dimensions = image.dimensions
+      image.manipulate!(:resize => "50%")
+      image.dimensions.map {|k,v| v.to_i}.should == original_dimensions.map {|k,v| v.to_i/2}
     end
 
     it "exposes width and height as integers" do
-      @image.width.should == 160
-      @image.height.should == 120
+      image.width.should == 160
+      image.height.should == 120
     end
   end
 
   describe "converting an image" do
     it "writes the new image out to a file" do
-      @image.convert("new_image.png")
+      image.convert("new_image.png")
       File.exists?("new_image.png").should be_true
     end
   end
 
   describe "identifying an image" do
     it "returns a list of layers" do
-      @image.identify.should include "JPEG 160x120"
+      image.identify.should include "JPEG 160x120"
     end
   end
 
--- spec/image_sorcery_spec.rb
+++ spec/image_sorcery_spec.rb
@@ -2,10 +2,5 @@ require 'fileutils'
 require 'image_sorcery'
 
 describe "Image Sorcery with ImageMagick" do
-  before :each do
-    FileUtils.copy "./spec/fixtures/dog.jpeg", "./spec/fixtures/dog-2.jpeg" 
-    @image = Sorcery.new("./spec/fixtures/dog-2.jpeg") # Who doesn't love dogs?
-  end
-  
-  it_behaves_like Sorcery
+  it_behaves_like Sorcery, "new"
 end
