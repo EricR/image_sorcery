90ce4a6cc03dbca7418a1e2d9925a17e3f60df5e
Eric Rafaloff <erafaloff@gmail.com>
2013-01-16 16:25:26 -0500

    Wow I'm really not with it today

--- spec/image_sorcery_gm_spec.rb
+++ spec/image_sorcery_gm_spec.rb
@@ -1,11 +1,6 @@
 require 'fileutils'
 require 'image_sorcery'
 
-describe "Image Sorcery with GraphicsMagick" do
-  before :each do
-    FileUtils.copy "./spec/fixtures/dog.jpeg", "./spec/fixtures/dog-2.jpeg"
-    @image = ImageSorcery.gm("./spec/fixtures/dog-2.jpeg") # Who doesn't love dogs?
-  end
-
-  it_behaves_like ImageSorcery, "gm"
+describe "Image Sorcery with ImageMagick" do
+  it_behaves_like ImageSorcery, "new"
 end
--- spec/image_sorcery_spec.rb
+++ spec/image_sorcery_spec.rb
@@ -1,11 +1,6 @@
 require 'fileutils'
 require 'image_sorcery'
 
-describe "Image Sorcery with ImageMagick" do
-  before :each do
-    FileUtils.copy "./spec/fixtures/dog.jpeg", "./spec/fixtures/dog-2.jpeg" 
-    @image = ImageSorcery.new("./spec/fixtures/dog-2.jpeg") # Who doesn't love dogs?
-  end
-  
-  it_behaves_like ImageSorcery
+describe "Image Sorcery with GraphicsMagick" do
+  it_behaves_like ImageSorcery, "gm"
 end
